8fae3148cef5604522786d463affd22a9ee15c2e
Eduard Dudin <edudin997@gmail.com>
2022-05-14 18:47:51 +0700

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import './styles/App.css'
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 
@@ -7,7 +8,15 @@ function App() {
 
   return (
     <div className="App">
-  <ClassCounter />
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>Javascript - Язык программирования</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   );
 }
